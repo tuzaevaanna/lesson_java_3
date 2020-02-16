148e63d4c7c94c6d82f2582c7d0f33b865a6a248
Tuzaeva Anna <tuzaeva.anna@mail.ru>
2020-02-16 13:30:23 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    private static Random rand = new Random();
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1"); guessTheNamber();
+    }
+
+
+    public static void guessTheNamber() {
+        int x = 1;
+        do {
+            int answer = rand.nextInt(10);
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("Угадайте число от 0 до 9: ");
+                int userAnswer = sc.nextInt();
+                if (userAnswer == answer) {
+                    System.out.println("Поздравляю, вы угадали!");
+                    break;
+                } else if (userAnswer > answer) {
+                    System.out.println("Вы ввели слишком большое число.");
+                } else if (userAnswer < answer) {
+                    System.out.println("Вы ввели слишком маленькое число.");
+                }
+                if (i == 3) {
+                    System.out.println("Вы проиграли!");
+                }
+            }
+            System.out.println("Попробовать еще раз? 1-да - 0-нет.");
+            int answer1 = sc.nextInt();
+            x = answer1;
+        } while (x == 1);
+        System.out.println("До встречи!");
+    }
 }
